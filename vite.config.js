4f0c6fb7278b0ca1dc572185f29c226ec8517adb
fltea <xm1025@foxmail.com>
2023-01-09 21:29:02 +0800

    feat: 修改项目配置

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import * as path from "node:path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    //设置别名
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+  // less 全局变量
+  css: {
+    preprocessorOptions: {
+      less: {
+        javascriptEnabled: true,
+        modifyVars: {
+          hack: `true; @import (reference) "${path.resolve(__dirname, "src/assets/css/var.less")}"`,
+        },
+      },
+    },
+  },
   plugins: [vue()],
-})
+});
