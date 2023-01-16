b060c681cb6bb85492178903d8aad81e272bafa7
fltea <xm1025@foxmail.com>
2023-01-16 16:58:52 +0800

    feat: 接口返回數據模型

---
+++ server/model/ResModel.js
@@ -0,0 +1,36 @@
+class BaseModel {
+  constructor({ code, data, message }){
+    this.code = code;
+    if(data) {
+      this.data = data;
+    }
+
+    if(message) {
+      this.message = message;
+    }
+  }
+}
+
+
+class SuccessModel extends BaseModel {
+  constructor(data = null) {
+    super({
+      code: 200,
+      data,
+    })
+  }
+}
+
+class ErrorModel extends BaseModel {
+  constructor({ code, message }){
+    super({
+      code,
+      message,
+    })
+  }
+}
+
+module.exports = {
+  SuccessModel,
+  ErrorModel,
+}
