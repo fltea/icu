5de7c3941545959feb5ed601aac2cc0f8214a43f
fltea <xm1025@foxmail.com>
2023-01-23 20:38:48 +0800

    fix: 去除重复代码

--- server/routes/index.js
+++ server/routes/index.js
@@ -56,31 +56,6 @@ const setRouter = (viteServer) => {
         console.log(e.stack);
         ctx.throw(500, e.stack);
       }
-      // serve index.html - we will tackle this next
-      try {
-        // 1. 获取index.html
-        let template = fs.readFileSync(resolve(process.cwd(), 'index.html'), 'utf-8');
-
-        // 2. 应用 Vite HTML 转换。这将会注入 Vite HMR 客户端，
-        template = await viteServer.transformIndexHtml(ctx.path, template);
-
-        // 3. 加载服务器入口, vite.ssrLoadModule 将自动转换
-        const { render } = await viteServer.ssrLoadModule('/src/entry-server.js');
-
-        //  4. 渲染应用的 HTML
-        const [renderedHtml] = await render(ctx);
-
-        const html = template.replace('<!--ssr-outlet-->', renderedHtml);
-
-        ctx.type = 'text/html';
-        ctx.body = html;
-      } catch (e) {
-        if (viteServer) {
-          viteServer.ssrFixStacktrace(e);
-        }
-        console.log(e.stack);
-        ctx.throw(500, e.stack);
-      }
     },
   );
 
