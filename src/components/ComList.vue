f8052233519bece34ee09057418d24a8acca9ab7
fltea <xm1025@foxmail.com>
2023-02-06 18:11:15 +0800

    feat: 新组件

---
+++ src/components/ComList.vue
@@ -0,0 +1,55 @@
+<script setup>
+import { ref, computed, onMounted, onBeforeUnmount } from 'vue';
+
+const props = defineProps({
+  finished: Boolean,
+  loading: Boolean,
+});
+const emit = defineEmits(['load']);
+const finished = computed(() => props.finished);
+const loading = computed(() => props.loading);
+
+const loadMain = ref(null);
+const observerHandle = (entries) => {
+  let nextLoad = entries[0].isIntersecting;
+  const Dom = loadMain.value;
+  // 判断是否一致
+  nextLoad = nextLoad && (Dom.offsetTop !== Dom.parentNode.offsetTop);
+  // 不在加载中
+  nextLoad = nextLoad && !loading.value;
+  // 不在已结束
+  nextLoad = nextLoad && !finished.value;
+  if (nextLoad) {
+    emit('load');
+  }
+};
+let observer;
+
+onMounted(() => {
+  console.log('onMounted');
+  observer = new IntersectionObserver(observerHandle);
+  observer.observe(loadMain.value);
+});
+onBeforeUnmount(() => {
+  if (observer) {
+    observer.unobserve(loading.value);
+    observer.disconnect();
+  }
+});
+</script>
+
+<template>
+<section class="com-list">
+  <slot></slot>
+  <div class="list-loading" ref="loadMain" v-if="!finished"></div>
+</section>
+</template>
+
+<style lang='less' scoped>
+.com-list {
+  overflow: auto;
+}
+.list-loading {
+  height: 30px;
+}
+</style>
