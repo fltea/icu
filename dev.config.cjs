8483ce0785a488ba7c83035dca7dc6c112056ac2
fltea <xm1025@foxmail.com>
2023-02-06 16:22:56 +0800

    fix: pm2监听文件夹 修改

--- dev.config.cjs
+++ dev.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
   apps: [{
     name: 'icu-dev',
     script: 'server/app.js',
-    watch: ['server', 'src'],
+    watch: ['server'],
     max_memory_restart: '3G',
     node_args: '--max_old_space_size=3072',
     log_date_format: 'YYYY/MM/DD HH:mm:ss',
