e0e2e0027e0ee0efb5277365ea8842771c8ecf62
fltea <xm1025@foxmail.com>
2023-02-08 16:56:06 +0800

    feat: 备份页面样式调整

--- src/pages/data.vue
+++ src/pages/data.vue
@@ -22,13 +22,20 @@ onMounted(() => {
 <template>
   <h1>Datas</h1>
   <button @click="backupData">新增备份</button>
-  <section>
-    <div v-for="item in listData" :key="`listData-${item}`">
-      <p>{{ item }}</p>
-      <button @click="restoreData(item)">还原备份</button>
+  <section class="list-container">
+    <div v-for="item in listData" :key="`listData-${item}`" class="list-item">
+      <p>{{ item }} <button @click="restoreData(item)">还原备份</button></p>
     </div>
   </section>
 </template>
 
 <style scoped lang="less">
+.list-container {
+  margin-top: 12px;
+  .list-item {
+    & +.list-item {
+      margin-top: 12px;
+    }
+  }
+}
 </style>
