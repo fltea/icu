96c25df1f62feb3a1319f98c713e61f36a645f2c
fltea <xm1025@foxmail.com>
2023-02-13 17:39:28 +0800

    feat: 通用上傳下載按鈕

---
+++ src/components/file/ComDownButton.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { computed } from 'vue';
+
+const props = defineProps({
+  action: String,
+});
+const action = computed(() => props.action);
+</script>
+
+<template>
+<div class="com-down-button">
+  <form class="down-form" :action="action" method="post" target="actionFrame">
+    <button type="submit"><slot></slot></button>
+    <iframe class="down-frame" name="actionFrame"></iframe>
+  </form>
+</div>
+</template>
+
+<style lang='less' scoped>
+.com-down-button {
+  display: inline-block;
+}
+.down-frame {
+  position: absolute;
+  bottom: 0;
+  opacity: 0;
+  pointer-events: none;
+}
+</style>
---
+++ src/components/file/ComUploadButton.vue
@@ -0,0 +1,19 @@
+<script setup>
+const emit = defineEmits(['change']);
+const changFile = (event) => {
+  emit('change', event);
+};
+</script>
+
+<template>
+<div class="com-upload-button">
+  <button class="file-button"><slot></slot><input type="file" name="file" @change="changFile"></button>
+</div>
+
+</template>
+
+<style lang='less' scoped>
+.com-upload-button {
+  display: inline-block;
+}
+</style>
