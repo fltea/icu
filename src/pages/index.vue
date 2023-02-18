405341b56097e1442e2da05c67c1dcc39246aa35
fltea <xm1025@foxmail.com>
2023-02-18 14:57:01 +0800

    feat: 清除首页内容

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,42 +1,10 @@
 <script setup>
-import { ref, onMounted } from 'vue';
-import { list } from '@/api/mark';
-
-const listData = ref([]);
-const newItem = () => {
-
-};
-// const changFile = (event) => {
-
-// };
-
-onMounted(() => {
-  list().then((res) => {
-    console.log(res);
-    listData.value.push('cesss');
-  });
-});
+// import { ref } from 'vue'
 </script>
 
 <template>
-  <h1>MARK</h1>
-  <section>
-    <button @click="newItem">新增</button>
-    <button class="file-button">批量导入<input type="file" name="file" id="myFile" @change="changFile"></button>
-  </section>
-  <section>
-    <!-- 列表 -->
-    <div>
-      <!-- icon -->
-      <div></div>
-      <!-- 内容 -->
-      <div></div>
-      <!-- 操作 -->
-      <div></div>
-    </div>
-  </section>
+<h1>INDEX</h1>
 </template>
 
-<style scoped lang="less">
-
+<style scoped lang='less'>
 </style>
