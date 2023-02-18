1c9ffd0cf975a81c2e6a3543b04ef867dafe17bc
fltea <xm1025@foxmail.com>
2023-02-18 20:28:23 +0800

    feat: 数据校验时 顺便处理数据

--- server/validator/_validate.js
+++ server/validator/_validate.js
@@ -3,11 +3,11 @@ import Ajv from 'ajv';
 const ajv = new Ajv();
 
 // YYYY-MM-DD hh:mm:ss
-ajv.addFormat('date-time', /^[0-9]{4}-[0-9]{1,2}-[0-9]{1,2}\s[0-9]{2}:[0-9]{2}:[0-9]{2}$/);
+ajv.addFormat('date-time', /(^$)|(^[0-9]{4}-[0-9]{1,2}-[0-9]{1,2}\s[0-9]{2}:[0-9]{2}:[0-9]{2}$)/);
 // YYYY-MM-DD
-ajv.addFormat('date', /^[0-9]{4}-[0-9]{1,2}-[0-9]{1,2}$/);
+ajv.addFormat('date', /(^$)|(^[0-9]{4}-[0-9]{1,2}-[0-9]{1,2}$)/);
 // hh:mm:ss
-ajv.addFormat('time', /^[0-9]{2}:[0-9]{2}:[0-9]{2}$/);
+ajv.addFormat('time', /(^$)|(^[0-9]{2}:[0-9]{2}:[0-9]{2})$/);
 
 function validate(schema, data = {}) {
   // 处理数据
@@ -31,6 +31,15 @@ function validate(schema, data = {}) {
       error: ajv.errors[0],
     };
   }
+  // 数据处理
+  list.forEach((key) => {
+    const item = rules[key];
+    const temp = data[key];
+    const keys = ['date-time', 'date', 'time'];
+    if (!temp && (item.belongsTo || keys.includes(item.format))) {
+      data[key] = null;
+    }
+  });
   return null;
 }
 
--- server/validator/index.js
+++ server/validator/index.js
@@ -2,66 +2,67 @@ import validate from './_validate.js';
 import { deepCopy } from '../utils/tools.js';
 import { types } from './type.js';
 
+function modedl2Schema(modeler, belongs) {
+  const belongsTo = Object.values(belongs);
+  const properties = {
+    id: {
+      type: 'number',
+    },
+  };
+  const required = [];
+  Object.keys(modeler).forEach((key) => {
+    const item = modeler[key];
+    if (item.allowNull === false) {
+      required.push(key);
+    }
+    const type = item.type.key || item.type;
+    const property = deepCopy(types[type]);
+    if (belongsTo.includes(type)) {
+      property.belongsTo = true;
+    }
+    properties[key] = property;
+  });
+
+  // 校验规则
+  return {
+    type: 'object',
+    properties,
+    if: {
+      properties: { id: { anyOf: [{ const: 0 }, { const: null }] } },
+    },
+    then: {
+      required,
+    },
+  };
+}
+
 async function getSchema(model) {
   let result = {};
   let modeler;
   try {
     // console.log('modeler');
     modeler = await import(`./model/${model}.js`);
-    if (modeler) {
-      modeler = modeler.default;
-    }
+    result = modedl2Schema(modeler.default);
   } catch (error) {
-    console.log('modeler error', error.code);
+    // console.log('modeler error', error.code);
     try {
       // 在数据库模型中
       modeler = await import(`../db/models/${model}.js`);
-      if (modeler) {
-        modeler = modeler.default.model;
-      }
+      modeler = modeler.default;
+      result = modedl2Schema(modeler.model, modeler.belongsTo || {});
     } catch (ee) {
-      console.log('modeler ee', ee.code);
+      // console.log('modeler ee', ee.code);
       result.error = `没有找到 model: ${model} SCHEMA 配置`;
     }
   }
 
-  // console.log('modeler', modeler);
-  if (modeler) {
-    const properties = {
-      id: {
-        type: 'number',
-      },
-    };
-    const required = [];
-    Object.keys(modeler).forEach((key) => {
-      const item = modeler[key];
-      if (item.allowNull === false) {
-        required.push(key);
-      }
-      const type = item.type.key || item.type;
-      properties[key] = deepCopy(types[type]);
-    });
-
-    // 校验规则
-    result = {
-      type: 'object',
-      properties,
-      if: {
-        properties: { id: { anyOf: [{ const: 0 }, { const: null }] } },
-      },
-      then: {
-        required,
-      },
-    };
-  }
-  // console.log('modeler result', result);
   return result;
 }
 
 async function validator(model, data = {}) {
   const SCHEMA = await getSchema(model);
   if (SCHEMA.error) {
-    return SCHEMA;
+    return null;
   }
   return validate(SCHEMA, data);
 }
