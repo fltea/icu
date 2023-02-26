f6cb63acb9c253c2434981175b5ead7a8d3620af
fltea <xm1025@foxmail.com>
2023-02-26 20:15:33 +0800

    feat: douban 页面导航

---
+++ src/pages/douban/group/index.vue
@@ -0,0 +1,17 @@
+<script setup>
+// import { ref } from 'vue';
+
+</script>
+
+<template>
+  <h1>GROUP</h1>
+  <div class="com-controls">
+    <button @click="addNewItems">链接小组</button>
+    <button @click="addNewItem">新增小组</button>
+  </div>
+  <section>
+  </section>
+</template>
+
+<style scoped lang='less'>
+</style>
