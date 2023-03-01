480bf4fd25abd784570f4009873f5418eb07427e
fltea <xm1025@foxmail.com>
2023-03-01 23:10:18 +0800

    feat: 增加事务回滚

--- server/db/seq.js
+++ server/db/seq.js
@@ -14,3 +14,17 @@ const conf = {
 const seq = new Sequelize(database, user, password, conf);
 
 export default seq;
+
+// 事务回滚
+export async function rollBack(promise, data) {
+  // 首先,我们开始一个事务并将其保存到变量中
+  const t = await seq.transaction();
+  try {
+    const result = await promise(data);
+    await t.commit();
+    return result;
+  } catch (error) {
+    await t.rollback();
+    return null;
+  }
+}
