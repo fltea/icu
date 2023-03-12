ca474e0f5d1f7bfebc3e9bdbf2bb18cf0a280b9d
fltea <xm1025@foxmail.com>
2023-03-12 13:10:08 +0800

    feat：增加错误类型

--- server/utils/tcatch.js
+++ server/utils/tcatch.js
@@ -2,11 +2,18 @@
  * @description 異常數據處理
  */
 import { ErrorModel } from '../model/ResModel.js';
-import { errorInfo } from '../model/ErrorInfos.js';
+import { errorInfo, timeoutInfo } from '../model/ErrorInfos.js';
 
 async function catchError(error) {
+  let info = errorInfo;
   console.log(error);
-  return new ErrorModel(errorInfo);
+  // console.log(error.name); // ReferenceError
+  // console.log(error.message); // lalala is not defined
+  // console.log(error.stack);
+  if (error.message.includes('timeout')) {
+    info = timeoutInfo;
+  }
+  return new ErrorModel(info);
 }
 
 export default catchError;
