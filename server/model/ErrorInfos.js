1bf90ee9ff21110c55158a55a36ab683fca117e2
fltea <xm1025@foxmail.com>
2023-03-12 13:08:02 +0800

    feat: 增加信息

--- server/model/ErrorInfos.js
+++ server/model/ErrorInfos.js
@@ -2,6 +2,10 @@ export const notExistInfo = {
   code: 404,
   message: '数据不存在',
 };
+export const errorInfo = {
+  code: 500,
+  message: '數據異常，请稍候再试',
+};
 export const isExistInfo = {
   code: 1000,
   message: '数据已存在',
@@ -26,16 +30,16 @@ export const uploadFileSizeFailInfo = {
   code: 10001,
   message: '上传文件尺寸过大',
 };
-export const errorInfo = {
-  code: 500,
-  message: '數據異常，请稍候再试',
+export const putOutInfo = {
+  code: 10002,
+  message: '被踢出',
 };
+export const timeoutInfo = {
+  code: 10003,
+  message: '请求超时',
+};
+
 export const fetchInfo = {
   code: 10011,
   message: '返回數據错误',
-};
-
-export const nodataInfo = {
-  code: 1004,
-  message: '暂无数据',
 };
