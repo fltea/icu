0943253b27fb8a3def6944b4cd5dafad9ff606de
fltea <xm1025@foxmail.com>
2023-03-16 21:30:16 +0800

    feat: 增加富文本编辑器

---
+++ src/pages/article/edit.vue
@@ -0,0 +1,20 @@
+<!-- eslint-disable import/no-extraneous-dependencies -->
+<script setup>
+import { ref } from 'vue';
+import MdEditor from 'md-editor-v3';
+import 'md-editor-v3/lib/style.css';
+
+const text = ref('');
+</script>
+
+<template>
+<section>
+  <md-editor v-model="text" />
+</section>
+</template>
+
+<style scoped lang='less'>
+.md-editor {
+  height: 600px;
+}
+</style>
---
+++ src/pages/article/index.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { ref, reactive } from 'vue';
+
+const curData = reactive({
+
+});
+
+const dialog = ref(false);
+
+const newArticle = () => {
+
+};
+</script>
+
+<template>
+  <h1>ARTICLE</h1>
+  <section class="com-controls">
+    <button @click="newArticle">新增</button>
+  </section>
+  <section>
+
+  </section>
+</template>
+
+<style scoped lang='less'>
+</style>
