e325988a9b45bac84af2035e4a13f5ad898d30ba
fltea <xm1025@foxmail.com>
2023-03-27 16:30:04 +0800

    feat: 修改文章編輯器配置

--- src/pages/article/edit.vue
+++ src/pages/article/edit.vue
@@ -5,16 +5,58 @@ import MdEditor from 'md-editor-v3';
 import 'md-editor-v3/lib/style.css';
 
 const text = ref('');
+const title = ref('');
+const toobars = [
+  'bold',
+  'underline',
+  'italic',
+  '-',
+  'title',
+  'strikeThrough',
+  'sub',
+  'sup',
+  'quote',
+  'unorderedList',
+  'orderedList',
+  'task',
+  '-',
+  'codeRow',
+  'code',
+  'link',
+  'image',
+  'table',
+  'mermaid',
+  'katex',
+  '-',
+  'revoke',
+  'next',
+  '=',
+  'pageFullscreen',
+  'fullscreen',
+  'preview',
+  'htmlPreview',
+  'catalog',
+  // 'github',
+  'save',
+];
 </script>
 
 <template>
 <section>
-  <md-editor v-model="text" />
+  <input class="title-input" type="text" v-model="title" />
+  <md-editor v-model="text" :toolbars="toobars" :preview="false"/>
 </section>
 </template>
 
 <style scoped lang='less'>
+.title-input {
+  margin: @tiny 0;
+  width: 100%;
+  height: 36px;
+  line-height: 34px;
+  font-size: 18px;
+}
 .md-editor {
-  height: 600px;
+  height: calc(100vh - 200px);
 }
 </style>
