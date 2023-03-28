d8b146b0aef4d439bcd4f2f2e15f5399e83b1632
fltea <xm1025@foxmail.com>
2023-03-28 18:21:13 +0800

    feat: 數據庫用戶頁面

--- src/pages/weibo/user.vue
+++ src/pages/weibo/user.vue
@@ -1,10 +1,44 @@
 <script setup>
-import { ref } from 'vue';
+import { reactive, onMounted } from 'vue';
+import { user } from '@/api/weibo';
+
+const list = reactive({
+  datas: [],
+});
+
+const listData = () => {
+  user().then((res) => {
+    console.log(res);
+    list.datas = res.data;
+  });
+};
+
+onMounted(listData);
 </script>
 
 <template>
-<section></section>
+<section class="list-container">
+  <div class="list-item" v-for="item in list.datas" :key="item.id">
+    <a :href="`/weibo/user/${item.id}`" target="_blank">{{item.screen_name}}</a>
+    <!-- <span class="list-del">刪除</span> -->
+  </div>
+</section>
 </template>
 
 <style lang='less' scoped>
+.list-container {
+  display: flex;
+  flex-wrap: wrap;
+  .list-item {
+    margin-bottom: 12px;
+    margin-right: 12px;
+    padding: 3px 6px;
+    background: #ccc;
+    border-radius: 4px;
+  }
+  .list-del {
+    margin-left: 12px;
+    font-size: 12px;
+  }
+}
 </style>
