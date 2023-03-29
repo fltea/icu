0918f516beb68bcc49be412a862c06ef70c58862
fltea <xm1025@foxmail.com>
2023-03-29 17:26:47 +0800

    feat: 保存微博數據列表

--- src/pages/weibo/record.vue
+++ src/pages/weibo/record.vue
@@ -1,10 +1,44 @@
 <script setup>
-import { ref } from 'vue';
+import { reactive, onMounted } from 'vue';
+import { record } from '@/api/weibo';
+
+const list = reactive({
+  datas: [],
+});
+
+const listData = () => {
+  record().then((res) => {
+    console.log(res);
+    list.datas = res.list;
+  });
+};
+
+onMounted(listData);
 </script>
 
 <template>
-<section></section>
+<section class="list-container">
+  <div class="list-item" v-for="item in list.datas" :key="item.id">
+    <a :href="item.authorLink" target="_blank">{{item.author}}</a>
+    <div v-html="item.content"></div>
+  </div>
+</section>
 </template>
 
 <style lang='less' scoped>
+.list-container {
+  display: flex;
+  flex-wrap: wrap;
+  .list-item {
+    margin-bottom: 12px;
+    margin-right: 12px;
+    padding: 3px 6px;
+    background: #ccc;
+    border-radius: 4px;
+  }
+  .list-del {
+    margin-left: 12px;
+    font-size: 12px;
+  }
+}
 </style>
