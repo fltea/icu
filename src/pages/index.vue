4f2ba5e1a68c4fc1391a72a942128094fea4776d
fltea <xm1025@foxmail.com>
2023-04-03 16:42:06 +0800

    feat: 增加自定义指令

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,9 +1,19 @@
 <script setup>
-// import { ref } from 'vue'
+import { ref } from 'vue';
+
+const loading = ref(false);
+
+const changValue = () => {
+  loading.value = !loading.value;
+};
 </script>
 
 <template>
 <h1>INDEX</h1>
+<section class="com-controls">
+  <button @click="changValue">修改值</button>
+</section>
+<section style="height:300px;" v-loading="loading"></section>
 </template>
 
 <style scoped lang='less'>
