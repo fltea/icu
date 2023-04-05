dcc45212a2e590988dc8b1d9b83fb131121711bc
fltea <xm1025@foxmail.com>
2023-04-05 21:50:53 +0800

    feat: 新增组件 DatePicker

--- src/components/DatePicker/index.vue
+++ src/components/DatePicker/index.vue
@@ -1,11 +1,40 @@
 <script setup>
-import { ref } from 'vue';
+import { computed } from 'vue';
+
+const props = defineProps({
+  placeholder: String,
+  format: String,
+  modelValue: String,
+});
+const emit = defineEmits(['update:modelValue', 'change']);
+
+const placestr = computed(() => props.placeholder || '');
+const datestr = computed({
+  get() {
+    const val = props.modelValue;
+    return val || '';
+  },
+  set(value) {
+    return emit('update:modelValue', value);
+  },
+});
 </script>
 
 <template>
-<section>
+<section class="date-picker">
+  <popover content="date-picker" trigger="focus">
+    <input type="text" :placeholder="placestr" v-model="datestr" class="date-input" readonly>
+    <template #content>
+      <p>content</p>
+    </template>
+  </popover>
 </section>
 </template>
 
 <style scoped lang='less'>
+.date-picker {
+  .date-input {
+    width: 100%;
+  }
+}
 </style>
