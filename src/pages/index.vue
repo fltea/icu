6e01238c4a58523c7b4aabd6602de30a06d6bf4b
fltea <xm1025@foxmail.com>
2023-04-12 21:24:30 +0800

    feat: 修改 404 页面

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,47 +1,11 @@
 <script setup>
-import { ref } from 'vue';
+// import { ref } from 'vue';
 
-const loading = ref(false);
-
-const changValue = () => {
-  loading.value = !loading.value;
-};
-
-const selectValue = ref('');
 </script>
 
 <template>
 <h1>INDEX</h1>
-<section class="com-controls">
-  <button @click="changValue">修改值</button>
-</section>
-<section class="components">
-  <div class="com-items loading-container" v-loading="loading">
-    <p class="items-title">loading</p>
-  </div>
-  <div class="com-items">
-    <p class="items-title">date-picker</p>
-    <date-picker></date-picker>
-  </div>
-
-  <div class="com-items" style="margin-top: 80px;">
-    <p class="items-title">com-select</p>
-    <com-select v-model="selectValue"></com-select>
-  </div>
-</section>
 </template>
 
 <style scoped lang='less'>
-.com-items {
-  margin-top: @small;
-}
-.items-title {
-  font-size: 20px;
-  line-height: 27px;
-  font-weight: bold;
-}
-.loading-container {
-  height: 80px;
-  background-color: antiquewhite;
-}
 </style>
