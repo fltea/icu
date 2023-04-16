64393046011d0be855191a4ac57c900ec3a19d1d
fltea <xm1025@foxmail.com>
2023-04-16 18:13:48 +0800

    feat: article 列表

--- src/pages/article/index.vue
+++ src/pages/article/index.vue
@@ -1,25 +1,79 @@
 <script setup>
-import { ref, reactive } from 'vue';
+import { reactive, onMounted } from 'vue';
+import { list } from '@/api/article';
+import { deepCopy } from '@/utils/tools';
 
 const curData = reactive({
-
+  list: [],
+  loading: false,
+  finished: false,
 });
+const search = reactive({
+  title: '',
+  content: '',
+});
+let params = null;
 
-const dialog = ref(false);
+// const dialog = ref(false);
 
-const newArticle = () => {
-
+const newArticle = (id) => {
+  window.open(`/article/${id || 'new'}`, 'articleEdit');
 };
+const loadList = () => {
+  curData.loading = true;
+  list(params).then((res) => {
+    // console.log(res, curData.list);
+    curData.list.push(...deepCopy(res.list) || []);
+    curData.finished = curData.list.length >= res.count;
+  }).finally(() => {
+    curData.loading = false;
+  });
+};
+const listMData = () => {
+  const page = params?.page || 1;
+  if (!params) {
+    params = {};
+  }
+  params.page = page + 1;
+  loadList();
+};
+const searchList = () => {
+  const { title, content } = search;
+  params = {
+    title,
+    content,
+  };
+  curData.list = [];
+  loadList();
+};
+const resetSearch = () => {
+  search.title = '';
+  search.content = '';
+  params = null;
+  curData.list = [];
+  loadList();
+};
+onMounted(loadList);
 </script>
 
 <template>
   <h1>ARTICLE</h1>
   <section class="com-controls">
-    <button @click="newArticle">新增</button>
+    <input type="text" v-model="search.title" placeholder="title">
+    <input type="text" v-model="search.content" placeholder="content">
+    <button @click="searchList">查詢</button>
+    <button @click="resetSearch">重設</button>
+    <button @click="newArticle()">新增</button>
   </section>
+  <com-list :finished="curData.finished" :loading="curData.loading" @load="listMData">
   <section>
-
+    <div class="list-item" v-for="(item, index) in curData.list" :key="`curData.list-${index}`">
+    <p>{{ item.title }}</p>
+    <p>{{ item.content }}</p>
+    <button @click="newArticle(item.id)">编辑</button>
+    </div>
   </section>
+  </com-list>
 </template>
 
 <style scoped lang='less'>
