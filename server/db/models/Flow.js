a13eb0c24374a552f439672ba8696ed1b52495ff
fltea <xm1025@foxmail.com>
2023-04-20 17:28:12 +0800

    feat: 修改flow數據結構

--- server/db/models/Flow.js
+++ server/db/models/Flow.js
@@ -30,18 +30,6 @@ const Flow = {
       type: FLOAT,
       comment: '价格',
     },
-    rete: {
-      type: FLOAT,
-      comment: '利率(%)',
-    },
-    beginDate: {
-      type: DATE,
-      comment: '开始时间',
-    },
-    endDate: {
-      type: DATE,
-      comment: '结束时间',
-    },
     name: {
       type: STRING,
       comment: '名称',
@@ -50,7 +38,7 @@ const Flow = {
       type: STRING,
       comment: '类型：股票 基金 定期 日用 工资 ……',
     },
-    means: {
+    property: {
       type: INTEGER,
       comment: '资产',
     },
