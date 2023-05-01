e283c25fc790200c3f195fd0bbb78f7301219460
fltea <xm1025@foxmail.com>
2023-05-01 17:50:53 +0800

    feat: 增各个表格类型常量

--- server/conf/constant.js
+++ server/conf/constant.js
@@ -17,6 +17,38 @@ export const COOKIES_DIR = `${dpath}/cookies`;
 // 默认 media 文件夹
 export const MEDIA_DIR = `${dpath}/media`;
 
+// 数据库 类型设定
+
+/**
+ * doulist 名称 id
+ * group 名称 id
+ * weibo 内容JSON weiboid 微博屏蔽
+ * follow 内容JSON id 地址 微博关注用户
+ * noveler 内容JSON 爬虫配置
+ */
+export const CRTYPE = {
+  DLIST: 'doubanDoulist',
+  DGROUP: 'doubanGroup',
+  WEIBO: 'weiboDetail',
+  WUSER: 'weiboUser',
+  WARTICLE: 'weiboArticle',
+  WBLOCK: 'weiboBlock',
+  NOVEL: 'noveler',
+};
+
+// flow type  类型: 支出0 收入1
+export const FLTYPE = '支出,收入';
+// flow tag 股票 基金 定期 吃 穿 住 行 通信(手机 网络) 红包 借款 工资 数码产品(手机 电脑 平板 相机) 旅游 贷款 教育 健康 娱乐
+export const FLTAG = '股票,基金,定期,吃,穿,住,行,通信(手机/网络等),红包,借款,工资,数码产品(手机/电脑/平板/相机等),旅游,贷款,教育,健康,娱乐';
+
+// media type 类型 书 小说 影片 电视剧 图片 视频
+export const METYPE = '书,小说,电影,电视,图片,视频';
+// media channel 渠道 douban weibo twitter
+export const MECHANNEL = '豆瓣,微博,推特,她乡';
+
+// Statistic type  类型: 月 年
+export const STTYPE = '月,年';
+
 // 請求設置
 export const UserAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36';
 
