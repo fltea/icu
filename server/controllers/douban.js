7057fafff348c83ac0dde193e00dc38fae5b04e7
fltea <xm1025@foxmail.com>
2023-05-07 21:50:06 +0800

    feat: 新增接口

--- server/controllers/douban.js
+++ server/controllers/douban.js
@@ -1,6 +1,5 @@
 import { schemaFileInfo, updateInfo, addInfo, delInfo } from '../model/ErrorInfos.js';
 import { ErrorModel, SuccessModel } from '../model/ResModel.js';
-import catchError from '../utils/tcatch.js';
 import { sleep } from '../utils/tools.js';
 import { setHashList } from '../utils/files.js';
 import { durlist, durl, gurlist, gurl, dDetail } from '../crawler/douban.js';
@@ -59,52 +58,44 @@ async function durlDetail(url, cookie, nolist) {
  * 根据 url 获取 豆列、小组
  */
 export async function getDurl({ cookie, url, nolist }) {
-  try {
-    let result = null;
-    // 豆列
-    if (url.includes('doulist')) {
-      if (url.includes('doulists')) {
-        result = await durlList(url, cookie);
-      } else {
-        result = await durlDetail(url, cookie, nolist);
-      }
-      return new SuccessModel(result);
+  let result = null;
+  // 豆列
+  if (url.includes('doulist')) {
+    if (url.includes('doulists')) {
+      result = await durlList(url, cookie);
+    } else {
+      result = await durlDetail(url, cookie, nolist);
     }
-
-    // 小组
-    if (url.includes('/group/')) {
-      if (url.includes('group/topic/')) {
-        result = await dDetail(url, cookie);
-      } else {
-        const urls = url.split('/');
-        const index = urls.findIndex((v) => v === 'group') + 1;
-        if (urls[index] === 'people' && urls.includes('joins')) {
-          // 豆瓣小组列表页面
-          result = await gurlist(url, cookie);
-        } else {
-          // 小组首页
-          result = await gurl(url, cookie, nolist);
-        }
-      }
-      return new SuccessModel(result);
-    }
-
-    return new ErrorModel(schemaFileInfo);
-  } catch (error) {
-    return catchError(error);
+    return new SuccessModel(result);
   }
+
+  // 小组
+  if (url.includes('/group/')) {
+    if (url.includes('group/topic/')) {
+      result = await dDetail(url, cookie);
+    } else {
+      const urls = url.split('/');
+      const index = urls.findIndex((v) => v === 'group') + 1;
+      if (urls[index] === 'people' && urls.includes('joins')) {
+        // 豆瓣小组列表页面
+        result = await gurlist(url, cookie);
+      } else {
+        // 小组首页
+        result = await gurl(url, cookie, nolist);
+      }
+    }
+    return new SuccessModel(result);
+  }
+
+  return new ErrorModel(schemaFileInfo);
 }
 
 /**
  * 根据 url 获取页面详情
  */
 export async function getDetail({ url, cookie }) {
-  try {
-    const result = await dDetail(url, cookie);
-    return new SuccessModel(result);
-  } catch (error) {
-    return catchError(error);
-  }
+  const result = await dDetail(url, cookie);
+  return new SuccessModel(result);
 }
 
 /**
@@ -115,73 +106,57 @@ export async function getDetail({ url, cookie }) {
  */
 const dtype = 'doubanDoulist';
 export async function getDoulist({ title, page, limit }) {
-  try {
-    const search = {
-      type: dtype,
-      content: title,
-      page,
-      limit,
-    };
-    const result = await clutters(search);
-    return new SuccessModel(result);
-  } catch (error) {
-    return catchError(error);
-  }
+  const search = {
+    type: dtype,
+    content: title,
+    page,
+    limit,
+  };
+  const result = await clutters(search);
+  return new SuccessModel(result);
 }
 
 /**
  * 获取豆列详情
  */
 export async function getDoulistById({ id }) {
-  try {
-    const result = await clutterInfo({ id });
-    return new SuccessModel(result);
-  } catch (error) {
-    return catchError(error);
-  }
+  const result = await clutterInfo({ id });
+  return new SuccessModel(result);
 }
 
 /**
  * 新增豆列
  */
 export async function setDoulist(doulist) {
-  try {
-    const clutter = {
-      type: dtype,
-      phrase: doulist.id,
-      content: JSON.stringify(doulist),
-    };
-    const result = await newClutter(clutter);
-    if (result) {
-      return new SuccessModel(result);
-    }
-    return new ErrorModel(addInfo);
-  } catch (error) {
-    return catchError(error);
+  const clutter = {
+    type: dtype,
+    phrase: doulist.id,
+    content: JSON.stringify(doulist),
+  };
+  const result = await newClutter(clutter);
+  if (result) {
+    return new SuccessModel(result);
   }
+  return new ErrorModel(addInfo);
 }
 
 /**
  * 修改豆列
  */
 export async function modDoulist(doulist) {
-  try {
-    const { clutter } = doulist;
-    ['clutter', 'phrase', 'type'].forEach((key) => {
-      delete doulist[key];
-    });
-    const item = {
-      content: JSON.stringify(doulist),
-      id: clutter,
-    };
-    const result = await changeClutter(item);
-    if (result) {
-      return new SuccessModel(result);
-    }
-    return new ErrorModel(updateInfo);
-  } catch (error) {
-    return catchError(error);
+  const { clutter } = doulist;
+  ['clutter', 'phrase', 'type'].forEach((key) => {
+    delete doulist[key];
+  });
+  const item = {
+    content: JSON.stringify(doulist),
+    id: clutter,
+  };
+  const result = await changeClutter(item);
+  if (result) {
+    return new SuccessModel(result);
   }
+  return new ErrorModel(updateInfo);
 }
 
 /**
@@ -189,92 +164,78 @@ export async function modDoulist(doulist) {
  */
 const gtype = 'doubanGroup';
 export async function getGroup({ name, page, limit }) {
-  try {
-    const search = {
-      type: gtype,
-      content: name,
-      page,
-      limit,
-    };
-    const result = await clutters(search);
-    return new SuccessModel(result);
-  } catch (error) {
-    return catchError(error);
-  }
+  const search = {
+    type: gtype,
+    content: name,
+    page,
+    limit,
+  };
+  const result = await clutters(search);
+  return new SuccessModel(result);
 }
 
 /**
  * 获取小组详情
  */
 export async function getGroupById({ id }) {
-  try {
-    const result = await clutterInfo({ id });
-    return new SuccessModel(result);
-  } catch (error) {
-    return catchError(error);
-  }
+  const result = await clutterInfo({ id });
+  return new SuccessModel(result);
 }
 
 /**
  * 新增小组
  */
 export async function setGroup(group) {
-  try {
-    if (group.tabs) {
-      group.tabs = JSON.parse(group.tabs);
-    }
-    if (group.tags) {
-      group.tags = JSON.parse(group.tags);
-    }
-    const clutter = {
-      type: gtype,
-      phrase: group.id,
-      content: JSON.stringify(group),
-    };
-    const result = await newClutter(clutter);
-    if (result) {
-      return new SuccessModel(result);
-    }
-    return new ErrorModel(addInfo);
-  } catch (error) {
-    return catchError(error);
+  if (group.tabs) {
+    group.tabs = JSON.parse(group.tabs);
   }
+  if (group.tags) {
+    group.tags = JSON.parse(group.tags);
+  }
+  const clutter = {
+    type: gtype,
+    phrase: group.id,
+    content: JSON.stringify(group),
+  };
+  const result = await newClutter(clutter);
+  if (result) {
+    return new SuccessModel(result);
+  }
+  return new ErrorModel(addInfo);
 }
 
 /**
  * 修改小组
  */
 export async function modGroup(group) {
-  try {
-    const { clutter } = group;
-    ['clutter', 'phrase', 'type'].forEach((key) => {
-      delete group[key];
-    });
-    const item = {
-      content: JSON.stringify(group),
-      id: clutter,
-    };
-    const result = await changeClutter(item);
-    if (result) {
-      return new SuccessModel(result);
-    }
-    return new ErrorModel(updateInfo);
-  } catch (error) {
-    return catchError(error);
+  const { clutter } = group;
+  ['clutter', 'phrase', 'type'].forEach((key) => {
+    delete group[key];
+  });
+  const item = {
+    content: JSON.stringify(group),
+    id: clutter,
+  };
+  const result = await changeClutter(item);
+  if (result) {
+    return new SuccessModel(result);
   }
+  return new ErrorModel(updateInfo);
 }
 
 /**
  * 删除豆列/小组
  */
 export async function delDouban({ clutter }) {
-  try {
-    const result = await deleteClutter(clutter);
-    if (result) {
-      return new SuccessModel(result);
-    }
-    return new ErrorModel(delInfo);
-  } catch (error) {
-    return catchError(error);
+  const result = await deleteClutter(clutter);
+  if (result) {
+    return new SuccessModel(result);
   }
+  return new ErrorModel(delInfo);
+}
+
+export async function setRecord({ detail }) {
+  const data = JSON.parse(detail);
+  console.log(data);
+  return new SuccessModel(data);
 }
